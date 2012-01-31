71a8b14a005da2392eb10c7504c3e5afc6ce85e0
perkins.tony <perkins.tony@gmail.com>
2012-01-31 22:12:26 +0000

    Tagging before major updates for 1.0 release.

--- agent/browser/chrome/extension/wpt/browserActionPopup.js
+++
@@ -1,8 +0,0 @@
-// Open a link from a browser action popup.  Normal links don't work,
-// because they would navigate in the popup itself.
-function openLink(url) {
-  chrome.tabs.create({
-      url: url,
-      selected: true
-  });
-}
